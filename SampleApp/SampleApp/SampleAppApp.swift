664733846e06cf5f8bf018837f647b32acfc051d
Jake Stephens <jake.stephens@raceroster.com>
2023-05-23 14:12:17 -0400

    Initial Commit
    - this is a sample app straight from xcode with no changes

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Jake Stephens on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
